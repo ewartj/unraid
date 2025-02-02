769943ccb08ef114eb8e4016a0918af1631530bd
ewartj <ewart.jonny@gmail.com>
2025-02-02 18:20:29 +0000

    made small fastapi to get the bin type

---
+++ bins/api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import uvicorn
+from datetime import date
+import yaml
+import os
+
+app = FastAPI()
+
+with open("config.yaml") as config:
+    try:
+        config = yaml.safe_load(config)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+@app.get("/bin")
+async def input():
+    today = date.today()
+    # Get the ISO calendar representation
+    week_number = date.today().isocalendar()[1]
+    # Extract the week number
+    # week_number = iso_calendar[1]
+    if week_number % 2 == 0:
+        bin = config["bins"]["even"]
+    else:
+        bin = config["bins"]["odd"]
+    return str(bin)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")#, port=int(os.getenv('APP_PORT')), host='0.0.0.0')
